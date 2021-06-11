110854638e8b4d28b12afb55bfda427511c8792f
Sam <sameer.agarwal.2295@gmail.com>
2021-06-11 13:24:01 +0200

    Finally

--- tests/test_raise_error_ex.py
+++ tests/test_raise_error_ex.py
@@ -2,17 +2,14 @@
 import torch
 #sys.path.insert(1,r'C:\Users\Sameer Agarwal\OneDrive - Danmarks Tekniske Universitet\Documents\Courses\DTU ML OPs\DTU_ML_OPs\Sameer_ML_Ops_Repo\src\models')
 from src.models.model import MyAwesomeModel
-import pytest
+import pytest    
+
 
 @pytest.mark.parametrize("x", [torch.rand([1,2,4,3]), torch.rand([1,2,3,3]), torch.rand([1,23,4,3])])
-#@pytest.mark.parametrize("x", [torch.rand([1,1,28,28]), torch.rand([1,1,28,28]), torch.rand([1,1,28,28])])
 def test(x):
     with pytest.raises(ValueError,match = r"Expected .*"):
-        test_call(x)
+        f(x)
 
-@pytest.mark.parametrize("x", [torch.rand([1,2,4,3]), torch.rand([1,2,3,3]), torch.rand([1,23,4,3])])
-#@pytest.mark.parametrize("x", [torch.rand([1,1,28,28]), torch.rand([1,1,28,28]), torch.rand([1,1,28,28])])
-def test_call(x):
-
+def f(x):
     model_test = MyAwesomeModel()
-    model_test.forward(x)
+    model_test.forward(x)   
