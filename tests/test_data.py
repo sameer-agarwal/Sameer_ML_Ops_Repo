f43abb1aa183a5fbfd12d1a4f601eb3c88324b11
Sam <sameer.agarwal.2295@gmail.com>
2021-06-11 11:34:41 +0200

    Correct path I hope

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,10 @@
 import sys
 
-sys.path.insert(1,r'C:\Users\Sameer Agarwal\OneDrive - Danmarks Tekniske Universitet\Documents\Courses\DTU ML OPs\DTU_ML_OPs\Sameer_ML_Ops_Repo\src\data')
+#sys.path.insert(1,r'C:\Users\Sameer Agarwal\OneDrive - Danmarks Tekniske Universitet\Documents\Courses\DTU ML OPs\DTU_ML_OPs\Sameer_ML_Ops_Repo\src\data')
+sys.path.insert(1,r'Sameer_ML_Ops_Repo\src\data')
 from make_dataset import main
 
-def check_size(images):
+pdef check_size(images):
     for i in range(len(images)):
         assert images[i].shape == (1,28,28)
 
