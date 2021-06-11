399ecfc8d28d4c8ae729bb2c859bb9b38b0e9b75
Sam <sameer.agarwal.2295@gmail.com>
2021-06-11 12:38:50 +0200

    Something with the fucking decorator

--- tests/test_raise_error_ex.py
+++ tests/test_raise_error_ex.py
@@ -1,15 +1,15 @@
-import sys
+#import sys
 import torch
-#sys.path.insert(1,r'C:\Users\Sameer Agarwal\OneDrive - Danmarks Tekniske Universitet\Documents\Courses\DTU ML OPs\04_Continuous_Integration\Structure_Example_repo\src\models')
+#sys.path.insert(1,r'C:\Users\Sameer Agarwal\OneDrive - Danmarks Tekniske Universitet\Documents\Courses\DTU ML OPs\DTU_ML_OPs\Sameer_ML_Ops_Repo\src\models')
 from src.models.model import MyAwesomeModel
 import pytest
 
-@pytest.mark.parametrize("x", [torch.rand(1,2,4,3), torch.rand(1,2,3,3), torch.rand(1,23,4,3)])
-
+@pytest.mark.parametrize("x", [torch.rand([1,2,4,3]), torch.rand([1,2,3,3]), torch.rand([1,23,4,3])])
 def test(x):
     with pytest.raises(ValueError,match = r"Expected .*"):
         test_call(x)
 
+@pytest.mark.parametrize("x", [torch.rand([1,2,4,3]), torch.rand([1,2,3,3]), torch.rand([1,23,4,3])])
 def test_call(x):
 
     model_test = MyAwesomeModel()
